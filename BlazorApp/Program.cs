4e0a8cf3fcd3627f996e503e259f72a001ac3d9b
glharper@microsoft.com <glharper@microsoft.com>
2023-03-16 13:28:49 -0700

    Add project files.

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,39 @@
+using BlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NewPolicy", builder =>
+     builder.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
+});
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseCors("NewPolicy");
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ BlazorApp/wwwroot/speechComponent.js
@@ -0,0 +1,44 @@
+﻿export function startRecognizer(key, region) {
+    const SpeechSDK = window.SpeechSDK;
+    if (!SpeechSDK) {
+        window.console.log("Speech SDK not loaded properly!");
+        return;
+    }
+
+    const config = SpeechSDK.SpeechConfig.fromSubscription(key, region);
+    const file = document.getElementById("audioFile").files[0];
+    const audioConfig = SpeechSDK.AudioConfig.fromWavFileInput(file);
+    const reco = new SpeechSDK.SpeechRecognizer(config, audioConfig);
+    const phraseDiv = document.getElementById("phraseDiv");
+    reco.recognizeOnceAsync(
+        function (result) {
+            window.console.log(result);
+
+            phraseDiv.innerHTML += "(continuation) Reason: " + SpeechSDK.ResultReason[result.reason];
+            switch (result.reason) {
+                case SpeechSDK.ResultReason.RecognizedSpeech:
+                    phraseDiv.innerHTML += " Text: " + result.text;
+                    break;
+                case SpeechSDK.ResultReason.NoMatch:
+                    var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
+                    phraseDiv.innerHTML += " NoMatchReason: " + SpeechSDK.NoMatchReason[noMatchDetail.reason];
+                    break;
+                case SpeechSDK.ResultReason.Canceled:
+                    var cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
+                    phraseDiv.innerHTML += " CancellationReason: " + SpeechSDK.CancellationReason[cancelDetails.reason];
+
+                    if (cancelDetails.reason === SpeechSDK.CancellationReason.Error) {
+                        phraseDiv.innerHTML += ": " + cancelDetails.errorDetails;
+                    }
+                    break;
+            }
+
+            phraseDiv.innerHTML += result.text + "\r\n";
+        },
+        function (err) {
+            window.console.log(err);
+
+            phraseDiv.innerHTML += "ERROR: " + err;
+        });
+}
+
